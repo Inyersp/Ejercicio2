5a989c01aa7e04afa9129bbd1e2438ec10801b72
Inyermansp <inyersp@gmail.com>
2022-11-26 16:04:27 -0500

    Ejercicio 2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Model
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            double numero1, numero2, numero3;
+            Console.WriteLine("Ingrese el primer numero");
+            numero1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero");
+            numero2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tercer numero");
+            numero3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("El promedio es: ");
+            double promedio = (numero1 + numero2 + numero3) / 3;
+            Console.WriteLine(promedio);
+
+        }
+
+
+    }
+}
